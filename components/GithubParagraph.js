df6fccc37b69cc31e140d89fe1d961a842bf1ae7
Joshua Culwell <getro1.josh@gmail.com>
2024-11-10 17:13:27 -0700

    Finished.

--- components/GithubParagraph.js
+++ components/GithubParagraph.js
@@ -4,6 +4,7 @@ export default function GithubParagraph() {
     return (
       <div>
         <p>https://github.com/JoshuaCulwell/NextJS</p>
+        <p>Web Topic: Making browser games. This topic is showcasing what is needed to create a browser game. To showcase this I'd like to make a game and discuss the development process.</p>
       </div>
     );
   }
